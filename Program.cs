c8901a47ac07931b17a2c2ca6dc5d8cb636ceb6c
Brandon000111 <5100143@mypillar7uwc.ac.za>
2023-11-28 12:47:40 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FirstclassExercise
+{
+
+
+    internal class Program
+    {
+        static bool IsSameForwardAndbackwards(string frontback)
+
+        { string backwardString = frontback;
+
+            
+            return false;
+        }
+        public static string reverseString(string forwardtext)
+        {
+            // Just for notes
+            for (int index = forwardtext.Length - 1; index > -1; index--) {
+                Console.Write(forwardtext[index]);
+            }
+            return "";
+        }
+
+        static void Main(string[] args)
+        {  
+            string reversedString = reverseString("racecar.");
+            Console.WriteLine(reversedString);
+        }
+    }
+
+}       
